42d08e4bbd60e77f82829c80ac94037a88c14cc1
Fan Sun <francis.sun@gmail.com>
2021-01-03 08:43:14 -0800

    fix for V1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
-        <!-- p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -18,7 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a -->
+        </a> */}
       </header>
     </div>
   );
